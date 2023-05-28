f5b9ef849306fed853b6862fe0f9ec4614ee6625
Shiv Singh <singhshiv0204@gmail.com>
2023-05-28 21:43:44 +0530

    Day 6 completed

---
+++ Live/Code/Module 2/Day 6 - Beginner Introduction to Beginner Module/Day_6.java
@@ -0,0 +1,119 @@
+/**
+ * Day_6
+ */
+public class Day_6 {
+
+  public static void main(String []args){
+  
+		// Your code goes here
+		
+		/*
+			Rules :
+			1. End your statements with semi colon (;)
+			2. Java is case sensitive
+			3. To print text we will use double quotes(" ")
+			4. print -> type the output
+			println -> type the output and press Enter	 
+			5. comments -> compiler ignores the comments
+			6. All the brackets are in pairs  -> { }, (), " "
+		*/
+		
+		// System.out.print(100);
+		
+		// System.out.print(10+20) 
+			// Error -> ; is missing
+			
+		// System.out.print(10+20); 
+			// 30
+		
+		// system.out.print(10+20); 
+			// Error: O should be small in out
+		
+		// System.out.print(10*20); 
+			// 200
+
+		// System.out.print(5-10); 
+			// -5 
+
+		// System.out.print("My name is Priyanshi");
+		
+		// System.out.print("Priyanshi);
+			// Error -> " is missing at the end
+
+		// System.out.print("10+20");
+			// 10+20
+
+		// Text : My phone number is 8938805908
+		// My email is priyanshi.sirohi@scaler.com
+		
+		
+		// System.out.prinT("10/20 world is ");
+			// Error: T should be small in print 	
+
+
+		// I love to code.
+		// Good morning
+		
+		// System.out.print("I love to code.");
+		// System.out.print("Good morning");
+			// I love to code.Good morning
+		
+		
+		
+		// System.out.println("I love to code.");
+		// System.out.println("Good morning");
+		
+		
+		
+		// System.out.print(10);
+		// System.out.print(20); 
+			//1020
+			
+			
+		// System.out.print("Scaler");
+		System.out.print("Academy");
+			// ScalerAcademy
+			
+		/*
+		//System.out.println("Super");
+  System.out.println("Excited");
+		   	//Excited
+		*/   
+		// Single Line Comments 
+		
+		
+		/*
+		This is 
+		a multi
+		line 
+		comments.
+		
+		*/
+		// System.out.print("Hey There");
+			//Hey There
+		
+		// System.out.print("A");
+		// System.out.println("B");
+		// System.out.println("C");
+		
+			/* output:
+				AB
+				C
+				
+			*/
+			
+		// System.out.print("A");
+		// System.out.println("B");
+		// System.out.println("C");
+		// System.out.print("D");
+		
+			/* output:
+				AB
+				C
+				D
+			*/
+		
+		
+	
+  }
+}
